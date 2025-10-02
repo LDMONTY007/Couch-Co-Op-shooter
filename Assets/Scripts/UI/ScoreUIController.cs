26ccb073eca0df1bee20e7a621a52b57f3bb24b2
LDMONTY007 <69697953+LDMONTY007@users.noreply.github.com>
2025-10-02 18:31:53 -0400

    Got the scale animation setup for the score UI controller

--- Assets/Scripts/UI/ScoreUIController.cs
+++ Assets/Scripts/UI/ScoreUIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -23,5 +24,50 @@ public class ScoreUIController : MonoBehaviour
     public void AddScore(int s)
     {
         score += s;
+
+        StartCoroutine(scaleCoroutine());
+    }
+
+    public IEnumerator scaleCoroutine()
+    {
+        float totalTime = 0.15f;
+        float curTime = 0f;
+
+        float startFontSize = scoreText.fontSize;
+        float MidFontSize = scoreText.fontSize + 16;
+
+        bool decreasing = false;
+        bool sentinel = true;
+
+        while (sentinel)
+        {
+            curTime += Time.deltaTime;
+
+            if (!decreasing)
+                scoreText.fontSize = Mathf.SmoothStep(startFontSize, MidFontSize, curTime / totalTime);
+            else
+                scoreText.fontSize = Mathf.SmoothStep(MidFontSize, startFontSize, curTime / totalTime);
+
+            //if we've reached the end of the lerp.
+            if (curTime >= totalTime)
+            {
+                if (decreasing == false)
+                {
+                    //start decreasing.
+                    decreasing = true;
+                    curTime = 0f;
+                }
+                else
+                {
+                    //stop the loop.
+                    sentinel = false;
+                }
+            }
+
+            yield return null;
+        }
+
+        //set back to start font size.
+        scoreText.fontSize = startFontSize;
     }
 }
